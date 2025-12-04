24db473a27d67211dc4131c79d4c578156e7ff1c
Kucharssim <kucharssim@gmail.com>
2025-12-04 15:30:18 +0100

    improve plots

--- R/geoms.R
+++ R/geoms.R
@@ -1,25 +1,40 @@
 StatPdf <- ggplot2::ggproto(
   "StatPdf", ggplot2::Stat,
   default_aes = ggplot2::aes(
-    x=NULL,
-    y = ggplot2::after_scale(y), ymin = ggplot2::after_stat(0), ymax = ggplot2::after_stat(y),
-    slope = ggplot2::after_stat(y), intercept = ggplot2::after_scale(intercept)),
+    x=NULL,xmin=NULL,xmax=NULL,
+    y = ggplot2::after_stat(density), ymin = ggplot2::after_stat(0), ymax = ggplot2::after_stat(density),
+    slope = ggplot2::after_stat(density), intercept = ggplot2::after_stat(intercept)),
   compute_group = function(data, scales, dist, log=FALSE, n=101, xlim=NULL, coverage=0.99) {
     if ("x" %in% colnames(data)) {
       x_vals <- data$x
-    } else if(!is.null(xlim)) {
-      x_vals = seq(xlim[1], xlim[2], length.out = n)
-    } else if (!is.null(scales$x)) {
-      xlim <- scales$x$get_limits()
-      x_vals = seq(xlim[1], xlim[2], length.out = n)
     } else {
-      tail <- (1-coverage) / 2
-      xlim <- qf(dist, c(tail, 1-tail))
-      x_vals <- seq(xlim[1], xlim[2], length.out = n)
+      if ("xmin" %in% colnames(data) && "xmax" %in% colnames(data)) {
+        xmin <- data[["xmin"]]
+        xmax <- data[["xmax"]]
+      } else if (!is.null(xlim)) {
+        xmin <- xlim[1]
+        xmax <- xlim[2]
+      } else if (!is.null(scales$x)) {
+        xlim <- scales$x$get_limits()
+        xmin <- xlim[1]
+        xmax <- xlim[2]
+      } else {
+        tail <- (1-coverage) / 2
+        xmin <- qf(dist, tail)
+        xmax <- qf(dist, 1-tail)
+      }
+
+      xlim <- scales$x$get_limits()
+
+      if (!is.finite(xmin) || xmin < xlim[1]) xmin <- xlim[1]
+      if (!is.finite(xmax) || xmax > xlim[2]) xmax <- xlim[2]
+
+      x_vals <- seq(xmin, xmax, length.out = n)
     }
-    y_vals <- pdf(dist, x_vals, log)
-    intercept <- cdf(dist, x_vals) - y_vals*x_vals
-    data.frame(x = x_vals, y = y_vals, intercept = intercept)
+
+    density <- pdf(dist, x_vals, log)
+    intercept <- cdf(dist, x_vals) - density*x_vals
+    data.frame(x = x_vals, density = density, intercept = intercept)
   }
 )
 
@@ -38,8 +53,8 @@ stat_pdf <- function(dist, mapping = NULL, data = NULL, geom = "point",
 
 StatCdf <- ggplot2::ggproto(
   "StatCdf", ggplot2::Stat,
-  default_aes = ggplot2::aes(x=NULL, y=ggplot2::after_scale(y)),
-  compute_group = function(data, scales, dist, lower.tail=TRUE, log.p=FALSE, n=101, xlim=NULL, coverage=0.99) {
+  default_aes = ggplot2::aes(xmin=NULL, xmax=NULL, y=ggplot2::after_scale(y)),
+  compute_group = function(data, scales, dist, log.p=FALSE, n=101, xlim=NULL, coverage=0.99) {
     if ("x" %in% colnames(data)) {
       x_vals <- data$x
     } else if(!is.null(xlim)) {
@@ -72,7 +87,9 @@ stat_cdf <- function(dist, mapping = NULL, data = NULL, geom = "point",
 
 StatCdfInterval <- ggplot2::ggproto(
   "StatCdfInterval", ggplot2::Stat,
-  default_aes = ggplot2::aes(x = ggplot2::after_stat(x_center), y = ggplot2::after_stat(y_center), label = ggplot2::after_stat(area)),
+  default_aes = ggplot2::aes(
+    xmin=NULL,xmax=NULL,
+    x = ggplot2::after_stat(x_center), y = ggplot2::after_stat(y_center), label = ggplot2::after_stat(probability)),
   compute_group = function(data, scales, dist, xlim=NULL, digits=3) {
     if ("xmin" %in% colnames(data)) {
       xmin <- data$xmin
@@ -90,7 +107,7 @@ StatCdfInterval <- ggplot2::ggproto(
       xmax <- dist@support@max
     }
 
-    area <- 1 - cdf(dist, xmin) - cdf(dist, xmax, lower.tail=FALSE)
+    probability <- 1 - cdf(dist, xmin) - cdf(dist, xmax, lower.tail=FALSE)
 
     if (!is.null(scales$x)) {
       plot_lim <- scales$x$get_limits()
@@ -98,37 +115,25 @@ StatCdfInterval <- ggplot2::ggproto(
       cli::cli_abort("stat_cdf_interval cannot itself determine plot limits")
     }
 
-    if (xmin < plot_lim[1]) {
-      xmin <- plot_lim[1]
-    } else if (xmin > plot_lim[2]) {
-      cli::cli_abort(
-        c("Stat interval is outside of the plot limits",
-          "i" = "The plot limits are {plot_lim}",
-          "x" = "The interval is {c(xmin, xmax)}")
-      )
-    }
+    if (any(xmin > xmax)) cli::cli_abort("Minimum of an interval must be smmaler than the maximum of an interval")
 
-    if (xmax > plot_lim[2]) {
-      xmax <- plot_lim[2]
-    } else if(xmax < plot_lim[1]) {
-      cli::cli_abort(
-        c("Stat interval is outside of the plot limits",
-          "i" = "The plot limits are {xlim}",
-          "x" = "The interval is {c(xmin, xmax)}")
-      )
-    }
+    # truncate the intervals to plotable area
+    xminplot <- ifelse(xmin < plot_lim[1], plot_lim[1], xmin)
+    xmaxplot <- ifelse(xmax > plot_lim[2], plot_lim[2], xmax)
 
-    x_vals <- seq(xmin, xmax, length.out=101)
-    y_vals <- pdf(dist, x_vals)
+    # for each interval, calculate the center of the plottable area
+    # this is done by calculating the weighted density
+    x_vals <- mapply(seq, from=xminplot, to=xmaxplot, length.out=101, SIMPLIFY = TRUE)
+    y_vals <- apply(x_vals, 2, function(x) pdf(dist, x))
 
-    x_center <- sum(x_vals * y_vals) / (sum(y_vals))
+    x_center <- colSums(x_vals * y_vals) / colSums(y_vals)
     y_center <- pdf(dist, x_center) / 2
 
-    data.frame(x_center=x_center, y_center=y_center, area=signif(area, digits=digits))
+    data.frame(x_center=x_center, y_center=y_center, probability=probability)
   }
 )
 
-stat_cdf_interval <- function(dist, mapping = NULL, data = NULL, geom = "text",
+stat_cdf_interval <- function(dist, mapping = NULL, data = NULL, geom = ggrepel::GeomLabelRepel,
                               position = "identity", ..., show.legend = NA, inherit.aes = TRUE) {
   if(is.null(data))
     data = ggplot2:::ensure_nonempty_data
--- R/plots.R
+++ R/plots.R
@@ -1,6 +1,3 @@
-
-
-
 S7::new_generic("plot", "x")
 plot_pdf <- S7::new_generic("plot_pdf", "distribution")
 plot_cdf <- S7::new_generic("plot_cdf", "distribution")
@@ -30,25 +27,38 @@ S7::method(plot, Distribution) <- function(x, type=c("all", "pdf", "cdf", "qf"),
   return(plot)
 }
 
-S7::method(plot_pdf, DistributionContinuous) <- function(distribution, xlim, points=NULL, intervals=list(), ...) {
+S7::method(plot_pdf, DistributionContinuous) <- function(distribution, ..., xlim, points=NULL, intervals=NULL) {
 
   plot <- ggplot2::ggplot() +
     ggplot2::xlim(x_range(distribution, xlim))
 
-  for (interval in intervals) {
+  if (!is.null(intervals)) {
+    intervals[[".group"]] <- seq_len(nrow(intervals))
     plot <- plot +
-      stat_pdf(n, geom="ribbon", fill="steelblue", xlim=interval) +
-      stat_cdf_interval(n, xlim=interval, mapping=ggplot2::aes(label = ggplot2::after_stat(area)))
+      stat_pdf(distribution, data=intervals,
+               mapping=ggplot2::aes(xmin=xmin, xmax=xmax, group=.group),
+               geom="ribbon", fill="steelblue", alpha=0.8) +
+      stat_cdf_interval(distribution, data=intervals,
+                        mapping=ggplot2::aes(xmin=xmin, xmax=xmax, group=.group,
+                                             label=ggplot2::after_stat(sprintf("%.3f", probability))
+                                             )
+      )
   }
-
 
   plot <- plot +
     stat_pdf(distribution, geom="line", linewidth=1.5)
 
-  if (!is.null(points))
+  if (!is.null(points)) {
+    df_points <- data.frame(x=points)
     plot <- plot +
-      stat_pdf(distribution, data=data.frame(x=points), mapping = ggplot2::aes(x=x),
-               size = 3, shape = 21, colour = "black", fill = "grey", alpha = NA, stroke = 0.5)
+      stat_pdf(distribution, data=df_points,
+               mapping = ggplot2::aes(x=x),
+               size = 3, shape = 21, colour = "black", fill = "grey", alpha = NA, stroke = 0.5) +
+      stat_pdf(distribution, data=df_points,
+               mapping = ggplot2::aes(x=x, label=ggplot2::after_stat(sprintf("%.3f", density))),
+               geom = ggrepel::GeomTextRepel
+               )
+  }
 
 
   return(plot)
@@ -65,102 +75,3 @@ S7::method(x_range, DistributionContinuous) <- function(distribution, xlim, cove
 
   return(xlim)
 }
-
-
-
-
-
-
-# plot_pdf <- S7::new_generic("plot_pdf", "distribution")
-#
-# S7::method(plot_pdf, distribution_continuous) <- function(distribution, x_range=qf(distribution, p=c(0.01, 0.99))) {
-#   plot <- ggplot2::ggplot() +
-#     jaspGraphs::scale_x_continuous(limits=x_range) +
-#     jaspGraphs::scale_y_continuous() +
-#     ggplot2::xlab("X") +
-#     ggplot2::ylab("Density")
-#
-#   result <- list(plot=plot, x_range=x_range)
-#
-#   class(result) <- c("plot_pdf", class(result))
-#   return(result)
-# }
-#
-# S7::method(plot_pdf, distribution_discrete) <- function(distribution, density=NULL, probability=NULL) {
-#   rlang::abort("Not implemented yet!")
-# }
-
-
-# plot_cdf <- S7::new_generic("plot_cdf", "distribution")
-#
-# S7::method(plot_cdf, distribution_continuous) <- function(distribution, x_range=qf(distribution, p=c(0.01, 0.99)), density=NULL, probability=NULL) {
-#   x_axis <- seq(x_range[1], x_range[2], length.out=101)
-#   y_axis <- cdf(distribution, x_axis)
-#
-#   plot <- ggplot2::ggplot() +
-#     ggplot2::geom_line(data = data.frame(x=x_axis, y=y_axis), mapping = ggplot2::aes(x=x, y=y), linewidth=1)
-#
-#   plot <- plot +
-#     jaspGraphs::scale_x_continuous() +
-#     ggplot2::scale_y_continuous(limits=c(0,1)) +
-#     ggplot2::xlab("X") +
-#     ggplot2::ylab("Cumulative probability")
-#
-#   class(plot) <- c("plot_cdf", class(plot))
-#   return(plot)
-# }
-#
-# plot_qf <- S7::new_generic("plot_qf", "distribution")
-#
-# S7::method(plot_qf, distribution_continuous) <- function(distribution, x_range=c(0.01, 0.99), ...) {
-#   x_axis <- seq(x_range[1], x_range[2], length.out=101)
-#   y_axis <- qf(distribution, x_axis)
-#
-#   plot <- ggplot2::ggplot() +
-#     ggplot2::geom_line(data = data.frame(x=x_axis, y=y_axis), mapping = ggplot2::aes(x=x, y=y), linewidth=1)
-#
-#   plot <- plot +
-#     jaspGraphs::scale_y_continuous() +
-#     jaspGraphs::scale_x_continuous() +
-#     ggplot2::xlab("Cumulative probability") +
-#     ggplot2::ylab("X")
-#
-#   return(plot)
-# }
-#
-# highlight_function <- S7::new_generic("highlight_function", c("plot", "distribution"))
-#
-# S7::method(highlight_function, list(S7::new_S3_class("plot_pdf"), distribution_continuous)) <- function(plot, distribution) {
-#   plot <- plot
-#     ggplot2::geom_line(data = data.frame(x=x_axis, y=y_axis), mapping = ggplot2::aes(x=x,y=y), linewidth=1)
-#   x_axis = seq(x_range[1], x_range[2], length.out=101)
-#   y_axis = pdf(distribution, x_axis)
-#   df = data.frame(x = x_axis, y = y_axis)
-# }
-#
-#
-# highlight_density <- S7::new_generic("highlight_density", c("plot", "distribution"))
-#
-# S7::method(highlight_density, list(S7::new_S3_class("plot_pdf"), distribution_continuous)) <- function(plot, distribution, x){
-#   df_points = data.frame(x=x, y=pdf(distribution, x))
-#   plot <- plot +
-#     ggplot2::geom_segment(
-#       data = data.frame(
-#         x = x, y = 0,
-#         xend = x, yend = df_points[["y"]]
-#       ),
-#       mapping = ggplot2::aes(x=x, y=y, xend=xend, yend=yend),
-#       linetype = 2
-#     ) +
-#     ggplot2::geom_segment(
-#       data = data.frame(
-#         x = x-1, y = df_points[["y"]],
-#         xend = x, yend = df_points[["y"]]
-#       ),
-#       mapping = ggplot2::aes(x=x, y=y, xend=xend, yend=yend),
-#       linetype = 2
-#     ) +
-#     jaspGraphs::geom_point(data = df_points, mapping = ggplot2::aes(x=x, y=y), size=2.5)
-#
-#   return(plot)
-# }
