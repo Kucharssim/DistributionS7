66509ced784b04f56821d9e4c0ec3b93d87695a4
Kucharssim <kucharssim@gmail.com>
2026-04-10 17:18:49 +0200

    add basic parameter_estimates tests

---
+++ tests/testthat/test-parameter-estimation.R
@@ -0,0 +1,27 @@
+testthat::test_that("parameter_estimates works", {
+  dist <- Normal(mu=0, sigma2=2)
+  x <- rng(dist, 100)
+  x_bar <- mean(x)
+
+  # analytic
+  pars <- parameter_estimates(dist, data=x, estimator=BiasCorrected())
+  testthat::expect_equal(
+    pars,
+    list(mu = x_bar, sigma2 = var(x))
+  )
+
+  # analytic mle
+  pars <- parameter_estimates(dist, data=x, estimator=Mle())
+  testthat::expect_equal(
+    pars, 
+    list(mu = x_bar, sigma2 = mean((x-x_bar)^2))
+  )
+
+  # optimizing mle
+  pars <- parameter_estimates(dist, data=x, estimator=Mle(optim=TRUE, start=list(mu=-1)))
+  testthat::expect_equal(
+    pars, 
+    list(mu = x_bar, sigma2 = mean((x-x_bar)^2)),
+    tolerance=1e-3
+  )
+})
