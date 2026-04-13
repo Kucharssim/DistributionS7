ecfc560c6f15d08f78b2c3810caf0271cb6a363d
Kucharssim <kucharssim@gmail.com>
2026-04-13 13:31:44 +0200

    add tests for parameter_inference

---
+++ tests/testthat/_snaps/parameter-estimation.md
@@ -0,0 +1,36 @@
+# parameter_inference works
+
+    Code
+      result
+    Output
+              key   label estimate       se     lower    upper
+      mu       mu    \\mu 1.390841 0.244828 0.8784101 1.903272
+      sigma sigma \\sigma 1.067180 0.168736 0.8326640 1.599186
+
+---
+
+    Code
+      result
+    Output
+              key   label estimate        se     lower    upper
+      mu       mu    \\mu 1.390841 0.2386288 0.9231371 1.858545
+      sigma sigma \\sigma 1.067180 0.1687359 0.7828006 1.454871
+
+---
+
+    Code
+      result
+    Output
+              key   label estimate        se     lower    upper
+      mu       mu    \\mu 1.390841 0.2386288 0.9231371 1.858545
+      sigma sigma \\sigma 1.067180 0.1687354 0.7364652 1.397896
+
+---
+
+    Code
+      result
+    Output
+              key   label estimate        se     lower    upper
+      mu       mu    \\mu 1.390840 0.2386290 0.9231359 1.858544
+      sigma sigma \\sigma 1.067181 0.1687362 0.7828010 1.454873
+
--- tests/testthat/test-parameter-estimation.R
+++ tests/testthat/test-parameter-estimation.R
@@ -24,4 +24,49 @@ testthat::test_that("parameter_estimates works", {
     list(mu = x_bar, sigma2 = mean((x-x_bar)^2)),
     tolerance=1e-3
   )
+
+  # constrained optimizing mle
+  pars <- parameter_estimates(dist, data=x, estimator=Mle(optim=TRUE, constrained=TRUE))
+  testthat::expect_equal(
+    pars, 
+    list(mu = x_bar, sigma2 = mean((x-x_bar)^2)),
+    tolerance=1e-3
+  )
+
+})
+
+testthat::test_that("parameter_inference works", {
+  dist <- Wald(mu=1, lambda=2)
+  x <- withr::with_seed(34698, rng(dist, 20))
+
+  # DefaultMethod switches to NormalTheory
+  testthat::expect_message(
+    parameter_inference(dist, DefaultMethod(args=list(constrained=TRUE)), data=x),
+    "Computing normal theory SE and CIs..."
+  )
+  
+  # and passess args forward
+  result_default <- parameter_inference(dist, DefaultMethod(args=list(constrained=TRUE)), data=x)
+  result_normal <- parameter_inference(dist, NormalTheory(constrained=TRUE), data=x)
+
+  testthat::expect_equal(result_default, result_normal)
+
+  
+  dist <- Normal(mu=0, sigma=1)
+  
+  # test analytic estimates + analytic ci
+  result <- parameter_inference(dist, DefaultMethod(), data=x)
+  testthat::expect_snapshot(result)
+
+  # analytic estimates + normal theory cis
+  result <- parameter_inference(dist, NormalTheory(), data=x)
+  testthat::expect_snapshot(result)
+
+  # analytic estimates + normal theory cis on constrained space
+  result <- parameter_inference(dist, NormalTheory(constrained=TRUE), data=x)
+  testthat::expect_snapshot(result)
+
+  # optim estimates + normal theory cis
+  result <- parameter_inference(dist, NormalTheory(estimator=Mle(optim=TRUE)), data=x)
+  testthat::expect_snapshot(result)
 })
